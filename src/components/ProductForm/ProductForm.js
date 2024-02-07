22b77e5bc1fe7da970755a9303e9102297209955
FedotovaOlga <olgaalex1994@gmail.com>
2024-02-07 16:53:30 +0100

    ProductForm connect to db

--- src/components/ProductForm/ProductForm.js
+++ src/components/ProductForm/ProductForm.js
@@ -1,45 +1,66 @@
-import React from 'react'
-
+import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
 const ProductForm = () => {
+  const [product, setProduct] = useState({});
+
+  const handleChange = (e) => {
+    setProduct({ ...product, [e.target.name]: e.target.value });
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch('http://localhost:4000/products', {
+      method: 'POST',
+      body: JSON.stringify(product),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => console.log("Success:", data))
+      .catch((error) => console.error("Error:", error));
+  }
+
+    
   return (
-    <Form>
-      <Form.Group className="mb-3" controlId="name">
+    <Form onSubmit={handleSubmit}>
+
+      <Form.Group className="m-3" controlId="name">
         <Form.Label>Name</Form.Label>
-        <Form.Control type="text" placeholder="Enter name" />
+        <Form.Control type="text" placeholder="Enter name" name="title" value={product.title || ''} onChange={handleChange} />
       </Form.Group>
 
-      <Form.Group className="mb-3" controlId="price">
+      <Form.Group className="m-3" controlId="price">
         <Form.Label>Price</Form.Label>
-        <Form.Control type="number" placeholder="Enter price" />
+        <Form.Control type="number" placeholder="Enter price" name="price" value={product.price || ''} onChange={handleChange} />
       </Form.Group>
 
-      <Form.Group className="mb-3" controlId="description">
+      <Form.Group className="m-3" controlId="description">
         <Form.Label>Description</Form.Label>
-        <Form.Control type="text" placeholder="Enter description" />
+        <Form.Control type="text" placeholder="Enter description" name="description" value={product.description || ''} onChange={handleChange} />
       </Form.Group>
 
-      <Form.Group className="mb-3" controlId="image">
+      <Form.Group className="m-3" controlId="image">
         <Form.Label>Image</Form.Label>
         <Form.Control type="text" placeholder="Enter image URL" />
       </Form.Group>
 
-      <Form.Group className="mb-3" controlId="category">
+      <Form.Group className="m-3" controlId="category">
         <Form.Label>Category</Form.Label>
         <Form.Control type="text" placeholder="Enter category" />
       </Form.Group>
 
-      <Form.Group className="mb-3" controlId="inStock">
+      <Form.Group className="m-3" controlId="inStock">
         <Form.Check type="checkbox" label="In stock" />
       </Form.Group>
 
-      <Form.Group className="mb-3" controlId="availableOnOrder">
+      <Form.Group className="m-3" controlId="availableOnOrder">
         <Form.Check type="checkbox" label="Available On Order" />
       </Form.Group>
 
-      <Form.Group className="mb-3" controlId="featured">
+      <Form.Group className="m-3" controlId="featured">
         <Form.Check type="checkbox" label="Featured" />
       </Form.Group>
 
